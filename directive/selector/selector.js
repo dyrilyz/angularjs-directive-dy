dd786ac75112f7bcce27c618d373d9fe635b7d41
rily <873023993@qq.com>
2018-05-23 11:21:14 +0800

    selector禁用效果下小箭头翻转bug修复

--- directive/selector/selector.js
+++ directive/selector/selector.js
@@ -24,7 +24,7 @@ angular.module('directive.selector', ['ngAnimate']).directive('dySelector', ['$d
             objectHandle: '='
         },
         template:
-            "<div class=\"dy-selector\"><div class=\"dy-selector-value\" ng-class=\"{'dy-selector-able':selectorAble}\" ng-click=\"showList()\"><span ng-if=\"ngModel.dyKey\" ng-bind=\"ngModel.dyKey\"></span><span ng-if=\"!ngModel.dyKey\" ng-bind=\"ngModel\"></span></div><span class=\"df df-down\" ng-class=\"{'rotate-up':isShowList}\"></span><div class=\"dy-selector-list\" ng-show=\"selectorAble && isShowList\"><div ng-hide=\"mode == 'simple'\"><input type=\"text\" ng-model=\"keyWord\"/><span class=\"df df-search-bold\"></span></div><ul><li ng-repeat=\"item in dyList track by $index\" ng-click=\"setItem(item)\" ng-show=\"itemShow(item)\"><span ng-if=\"item.dyKey\" ng-bind=\"item.dyKey\"></span><span ng-if=\"!item.dyKey\" ng-bind=\"item\"></span></li></ul></div></div>",
+           "<div class=\"dy-selector\"><div class=\"dy-selector-value\" ng-class=\"{'dy-selector-able':selectorAble}\" ng-click=\"showList()\"><span ng-if=\"ngModel.dyKey\" ng-bind=\"ngModel.dyKey\"></span><span ng-if=\"!ngModel.dyKey\" ng-bind=\"ngModel\"></span></div><span class=\"df df-down\" ng-class=\"{'rotate-up': selectorAble && isShowList}\"></span><div class=\"dy-selector-list\" ng-show=\"selectorAble && isShowList\"><div ng-hide=\"mode == 'simple'\"><input type=\"text\" ng-model=\"keyWord\"/><span class=\"df df-search-bold\"></span></div><ul><li ng-repeat=\"item in dyList track by $index\" ng-click=\"setItem(item)\" ng-show=\"itemShow(item)\"><span ng-if=\"item.dyKey\" ng-bind=\"item.dyKey\"></span><span ng-if=\"!item.dyKey\" ng-bind=\"item\"></span></li></ul></div></div>",
         // 开发模式dom如下
         //     `
         // <div class="dy-selector">
@@ -32,7 +32,7 @@ angular.module('directive.selector', ['ngAnimate']).directive('dySelector', ['$d
         //         <span ng-if="ngModel.dyKey" ng-bind="ngModel.dyKey"></span>
         //         <span ng-if="!ngModel.dyKey" ng-bind="ngModel"></span>
         //     </div>
-        //     <span class="df df-down" ng-class="{'rotate-up':isShowList}"></span>
+        //     <span class="df df-down" ng-class="{'rotate-up': selectorAble && isShowList}"></span>
         //     <div class="dy-selector-list" ng-show="selectorAble && isShowList">
         //         <div ng-hide="mode == 'simple'">
         //             <input type="text" ng-model="keyWord"/>
